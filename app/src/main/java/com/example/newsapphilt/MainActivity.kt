a36a0cc58ea9e004b89df61eec691e6693d02af2
Nur <qalay84@gmail.com>
2022-06-30 15:15:36 +0500

    added splash screen

--- app/src/main/java/com/example/newsapphilt/MainActivity.kt
+++ app/src/main/java/com/example/newsapphilt/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.newsapphilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import com.example.newsapphilt.presentation.splash_screen.SplashScreenFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.start_fragment_container, SplashScreenFragment())
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/newsapphilt/presentation/splash_screen/SplashScreenFragment.kt
@@ -0,0 +1,68 @@
+package com.example.newsapphilt.presentation.splash_screen
+
+import android.animation.Animator
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.airbnb.lottie.LottieAnimationView
+import com.example.newsapphilt.R
+import com.example.newsapphilt.presentation.main_fragment.MainFragment
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenFragment : Fragment() {
+
+    private lateinit var lottieAnimationView: LottieAnimationView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+        val view = inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        init(view)
+        listener()
+        return view
+    }
+
+
+    private fun listener() {
+        lottieAnimationView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                requireActivity()
+                    .supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.start_fragment_container, MainFragment())
+                    .commit()
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+            }
+
+            override fun onAnimationRepeat(p0: Animator?) {
+            }
+
+        })
+    }
+
+    private fun init(view: View) {
+        lottieAnimationView = view.findViewById(R.id.lottie_animation)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+
+    }
+}
